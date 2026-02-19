e0bf986dfb4993e5e482a2287d58510a8d450ce8
shelton <damwcs@gmail.com>
2026-02-19 13:25:04 +1100

    setup db connection

--- app/main.py
+++ app/main.py
@@ -1,23 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from sqlmodel import SQLModel
 
-app = FastAPI()
+from app.database import engine
 
+app = FastAPI(title="Task Management API")
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+SQLModel.metadata.create_all(engine)
+print("Database connected and tables created")
